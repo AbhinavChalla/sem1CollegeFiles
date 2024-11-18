fce00910e47ebe09393efa845828ac438133acc6
AbhinavChalla <63104658+AbhinavChalla@users.noreply.github.com>
2024-11-18 22:51:05 +0530

    Finding the least weight path
    Not yet done

---
+++ pathfinding.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int comb(int n, int r)
+{
+    if(n == r - 1 || r == 1)
+        return n;
+    else if(n == r || r == 0)
+        return 1;
+    return comb(n - 1, r) + comb(n - 1, r - 1);
+}
+
+int findPath(int arr[4][4], int rows, int cols)
+{
+    char path[/*rows + cols - 2*/6];
+    //int /*numberOfPaths*/ = comb(/*rows + cols - 2*/6, ((rows > cols) ? cols - 1 : rows - 1));
+    int sumInPath[/*numberOfPaths*/20] = {0};
+    for(int i = 0; i < /*rows + cols - 2*/6; i++) {
+        for(int j = i + 1; j < /*rows + cols - 2*/6
+; j++) {
+            for(int k = j + 1; k < /*rows + cols - 2*/6
+    ; k++) {
+                path[i] = 'r';
+                path[j] = 'r';
+                path[k] = 'r';
+                for(int l = 0; l < /*rows + cols - 2*/6
+        ; l++)
+                    if(path[l] != 'r')
+                        path[l] = 'd';
+                int count = 0;
+                int *ptr = &arr[0][0];
+                while(count < /*numberOfPaths*/20) {
+                    for(char step : path) {
+                        if(step == 'r') {
+                            cout << *ptr << "\n";
+                            sumInPath[count] += *ptr;
+                            cout << sumInPath[count] << "\n";
+                            ptr++;
+                        } else if(step == 'd') {
+                            cout << *ptr << "\n";
+                            sumInPath[count] += *ptr;
+                            cout << sumInPath[count] << "\n";
+                            ptr += cols;
+                        }
+                    }
+                    count++;
+                }
+            }
+        }
+    }
+    int shortestPathValue = sumInPath[0];
+    for(int i = 1; i < /*numberOfPaths*/20; i++)
+        cout << sumInPath[i] << "\n";
+    return shortestPathValue;
+}
+
+int main()
+{
+    int arr[4][4] = {
+        {1, 3, 5, 8},
+        {4, 2, 1, 7},
+        {4, 3, 2, 3},
+        {5, 6, 1, 4}
+    };
+
+    cout << "The value of the sum in the shortest path is " << findPath(arr, 4, 4);
+}
